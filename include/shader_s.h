5c201c91c22d3ba111b25739e37622181d377f38
linbinghe <linbinghe@gmail.com>
2018-07-12 21:29:19 +0800

    光照——颜色章节

--- include/shader_s.h
+++ include/shader_s.h
@@ -100,6 +100,14 @@ public:
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
     }
 
+    void setVec3(const std::string &name, const glm::vec3 &value) const {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+
+    void setVec3(const std::string &name, float x, float y, float z) const {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+    }
+
 private:
     // 检测异常
     // ------------------------------------------------------------------------
